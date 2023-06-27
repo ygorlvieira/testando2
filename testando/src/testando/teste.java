9208982e6862cbcd172fcbbd378d70293e595706
Ygor Vieira <115748634+ygorlvieira@users.noreply.github.com>
2023-06-27 10:43:27 -0300

    Update teste.java

--- testando/src/testando/teste.java
+++ testando/src/testando/teste.java
@@ -3,7 +3,7 @@ package testando;
 public class teste {
 
 	public static void main(String[] args) {
-		System.out.println("Eu sou o mior");
+		System.out.println("@@##@#@#@#");
 //lihas da main
 	}
 
